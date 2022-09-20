c1ea556500fcb65e21a5175e65a199e89ea4709c
Stausssi <stauss.simon.01@gmail.com>
2022-09-21 00:17:53 +0200

    Delete install.sh

--- install.sh
+++
@@ -1,6 +0,0 @@
-cp -R ./runnable ./infrared-controller
-cp -R ./infrared-controller /var/opt
-rm -rf ./infrared-controller
-
-chown -R pi:pi /var/opt/infrared-controller
-chmod +x /var/opt/infrared-controller/runnable.py
