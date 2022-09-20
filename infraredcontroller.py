9c11f6f76f6966f637d828618e607a8989d753bd
Stausssi <stauss.simon.01@gmail.com>
2022-09-20 23:05:48 +0200

    removed abstract parent

--- infraredcontroller.py
+++ infraredcontroller.py
@@ -1,12 +1,26 @@
-from runnable_class import Runnable
+import json
+import sys
+from threading import Thread
 
 
-class InfraredController(Runnable):
+class InfraredController:
     def __init__(self):
         print("Init")
 
-    def __handle_message(self, message):
+    def run(self):
+        thread = Thread(target=self.loop)
+        thread.start()
+
+        thread.join()
+
+    def loop(self):
+        for line in sys.stdin:
+            print(f"Received {line.strip()}")
+            self.handle_message(json.loads(line))
+
+    def handle_message(self, message):
         print(f"Received the message: {message}")
 
     def send(self, message):
-        super().send(message)
+        sys.stdout.write(json.dumps(message) + "\n")
+        sys.stdout.flush()
